5686e14469ccaa47fe8142ee66763c4cebc006e3
Christofer Engel <christofer.engel@gmail.com>
2026-01-08 13:27:26 +0100

    Updated version

--- server/const.go
+++ server/const.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	// Version is the current version for the server.
-	Version = "1.8.6"
+	Version = "1.8.7"
 
 	// ProtocolVersion is the implemented RES protocol version.
 	ProtocolVersion = "1.2.3"
